3755948e215f470bdf4a27c4e90cd2a4388e32f3
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-08-07 23:39:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,40 +1,35 @@
 import os
 
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
 
-    # ---------------------------------
-    # Security
-    # ---------------------------------
-
     SECRET_KEY = os.environ.get(
-        "SECRET_KEY"
+        "SECRET_KEY",
+        "change-this-later"
     )
-
-    # ---------------------------------
-    # Database
-    # ---------------------------------
 
     DATABASE_URL = os.environ.get("DATABASE_URL")
 
     if DATABASE_URL:
-        # Render PostgreSQL
-        if DATABASE_URL:
+
         if DATABASE_URL.startswith("postgres://"):
+
             DATABASE_URL = DATABASE_URL.replace(
                 "postgres://",
                 "postgresql://",
                 1
             )
+
         SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     else:
-        # Local development SQLite
+
         SQLALCHEMY_DATABASE_URI = (
-            "sqlite:///"
-            + os.path.join(
+            "sqlite:///" +
+            os.path.join(
                 BASE_DIR,
                 "instance",
                 "cable_quality.db"
@@ -42,4 +37,3 @@ class Config:
         )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
