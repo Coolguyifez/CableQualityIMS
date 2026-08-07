e9a30c1c633eec900c89927d51a42909975d6c18
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-08-08 00:03:30 +0100

    Create deploy.py

---
+++ deploy.py
@@ -0,0 +1,9 @@
+from app import create_app
+from app.extensions import db
+from flask_migrate import upgrade
+
+app = create_app()
+
+with app.app_context():
+    upgrade()
+    print("Database migrations completed successfully.")
