d7cc6786fae35f40b3ce8ca6c0add9bc53116a61
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-08-08 01:32:19 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,9 +1,56 @@
+from flask import current_app
+from flask_migrate import upgrade
 from app import create_app
 from app.extensions import db
-from flask_migrate import upgrade
+from app.models import Company, User
 
 app = create_app()
 
 with app.app_context():
-    upgrade()
+
+    # Run database migrations
+    upgrade
     print("Database migrations completed successfully.")
+
+    # Create system company
+    company = Company.query.filter_by(
+        company_code="SYS"
+    ).first()
+
+    if not company:
+        company = Company(
+            company_name="CableQIMS",
+            company_code="SYS",
+            email="admin@cableqims.com",
+            is_active=True
+        )
+
+        db.session.add(company)
+        db.session.commit()
+
+        print("Platform company created.")
+
+    # Create system administrator
+    admin = User.query.filter_by(
+        username="superadmin"
+    ).first()
+
+    if not admin:
+        admin = User(
+            company_id=company.id,
+            full_name="System Administrator",
+            username="superadmin",
+            email="admin@cableqims.com",
+            role="System Administrator",
+            is_active=True
+        )
+
+        admin.set_password("Admin@123")
+
+        db.session.add(admin)
+        db.session.commit()
+
+        print("System Administrator created.")
+
+    else:
+        print("System Administrator already exists.")
