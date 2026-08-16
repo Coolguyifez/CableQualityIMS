2207f71f8d1baa293d8f280c1899a285549327e8
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-08-16 19:35:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,22 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
+    VAPID_PRIVATE_KEY = os.environ.get(
+        "VAPID_PRIVATE_KEY"
+        
+    )
+
+    VAPID_PUBLIC_KEY = os.environ.get(
+        "VAPID_PUBLIC_KEY"
+        
+    )
+
+    VAPID_CLAIM_EMAIL = os.environ.get(
+        "VAPID_CLAIM_EMAIL"
+        
+    )
+
+
     SECRET_KEY = os.environ.get(
         "SECRET_KEY"
     )
