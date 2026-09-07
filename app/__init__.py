0cdd0e4d8ed72f38c805cf6ec63b0db91f01fceb
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-09-07 13:34:12 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -144,5 +144,21 @@ def create_app():
             "latest_notifications": latest_notifications
         }
 
+    # error message handler
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        db.session.rollback()
+
+        return render_template(
+            "errors/500.html"
+        ), 500
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template(
+            "errors/404.html"
+        ), 404
+
+
     return app
 
